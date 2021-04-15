9f1faefe040c5cf98c7f4d01e86c8e46c8f91e39
Derek Roberts <derek.roberts@gmail.com>
2021-04-15 13:59:26 -0700

    Login cmd now synchronous to pass tests

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -1,28 +1,27 @@
-import { Command, flags } from "@oclif/command";
-let cmd = require("node-cmd");
+import {Command} from '@oclif/command'
 
 export default class Login extends Command {
   static description =
-    "Checks for oc login.  If necessary, provides a token link.";
+  'Checks for oc login.  If necessary, provides a token link.';
 
   static examples = [
     `
-$ bcoc login
-> Logged in as: <USERNAME>
-`,
+    $ bcoc login
+    > Logged in as: <USERNAME>
+    `,
   ];
 
   async run() {
-    cmd.run(`oc whoami`, function (err, data, stderr) {
-      if (data) {
-        console.log("\nLogged in as:", data);
-      } else {
-        console.log(
-          "\nNot logged in.  Visit the link below to obtain a login token.",
-          "\nhttps://oauth-openshift.apps.silver.devops.gov.bc.ca/oauth/token/request",
-          "\n"
-        );
-      }
-    });
+    const cmd = require('node-cmd')
+    const {data} = cmd.runSync('oc whoami')
+    if (data) {
+      console.log('\nLogged in as:', data)
+    } else {
+      console.log(
+        '\nNot logged in.  Visit the link below to obtain a login token.',
+        '\nhttps://oauth-openshift.apps.silver.devops.gov.bc.ca/oauth/token/request',
+        '\n'
+      )
+    }
   }
 }
