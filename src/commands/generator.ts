fb041f1683595d07a076d62df7e26c3b3b34fdff
Derek Roberts <derek.roberts@gmail.com>
2021-04-16 14:50:06 -0700

    generator - take save file as parameter

--- src/commands/generator.ts
+++ src/commands/generator.ts
@@ -3,6 +3,8 @@ import {Command} from '@oclif/command'
 export default class Generator extends Command {
   static description = 'Generates base configuration for use with bcoc'
 
+  static args = [{name: 'envFile'}]
+
   async run() {
     // https://www.npmjs.com/package/inquirer
     const inquirer = require('inquirer')
@@ -65,10 +67,13 @@ export default class Generator extends Command {
       },
     ]
 
+    const saveFile = this.parse(Generator).args.envFile
+
     inquirer.prompt(questions).then(answers => {
-      console.log('\n', answers)
+      console.log(`\nSave to ${saveFile}:`, answers)
+      console.log('Press enter to save or Ctrl+C to exit.')
       require('fs').writeFile(
-        'bcoc.env',
+        this.parse(Generator).args.envFile ?? 'bcoc.env',
         JSON.stringify(answers, null, 2),
         err => !err || console.log(err)
       )
